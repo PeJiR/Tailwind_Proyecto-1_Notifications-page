4c2eb15d4c9b1b581474a8be8ebda15f3116049c
pejir <pejir84@gmail.com>
2024-04-10 00:04:49 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 const darkModeInput = document.querySelector('#darkmode');
 
 darkModeInput.addEventListener('click',()=>{
-    document.documentElement.classList.toggle('dark');
+ document.documentElement.classList.toggle('dark');
 })
+
+const markAll = document.querySelector("#mark-all");
+const numberElement = document.querySelector("#number");
+const posts = document.querySelectorAll(".post");
+
+posts.forEach((post) => {
+  post.addEventListener("click", () => {
+    post.querySelector(".not-read").classList.remove("not-read");
+    update();
+  });
+});
+
+markAll.addEventListener("click", () => {
+  const notReadElements = document.querySelectorAll(".not-read");
+
+  notReadElements.forEach((notReadElement) => {
+    notReadElement.classList.remove("not-read");
+  });
+  update();
+});
+
+const update = () => {
+  const notReadElementsCurrent = document.querySelectorAll(".not-read");
+
+  numberElement.innerText = [" "] + notReadElementsCurrent.length + [" left "];
+};
